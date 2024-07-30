34e3b61cf65e1bd46ab2cd71c8ac07ee7e50bef6
horiryo <horiryo@horiryo>
2024-07-31 01:00:21 +0900

    add auth

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "23cdbf586482fe8e72a1e7cba9616cce",
+const RESOURCES = {"flutter_bootstrap.js": "37057bd7371d4dd77a735a192b20cb39",
 "version.json": "5a6c576595ea4ca9e4cdb15d25e94d15",
 "index.html": "5c5b9160c1eb6c9604b3eaefb721da4b",
 "/": "5c5b9160c1eb6c9604b3eaefb721da4b",
-"main.dart.js": "faa7f065bb595f58841e6861c91deddb",
+"main.dart.js": "ea47068cd8bf66f7fe40d83616c8a5ab",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
