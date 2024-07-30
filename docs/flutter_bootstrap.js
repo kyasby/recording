13148e843c4dc330614f5c6fb11f438c6d2d407b
horiryo <horiryo@horiryo>
2024-07-30 23:45:10 +0900

    revise

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4244031233"
+    serviceWorkerVersion: "230234811"
   }
 });
